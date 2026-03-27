7c9f9f99b68c129c392d17267610277e812f825c
n.melnikov <n.melnikov@depra.org>
2026-03-27 21:39:05 +0400

    [v0.0.4] Small upgrade for player prefab selection

--- Runtime/CampaignCompletion.cs
+++ Runtime/CampaignCompletion.cs
@@ -1,7 +1,9 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
-// © 2024-2025 Depra <n.melnikov@depra.org>
+// © 2024-2026 Depra <n.melnikov@depra.org>
 
 using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Depra.Campaign
 {
@@ -12,6 +14,23 @@ namespace Depra.Campaign
 		public CampaignCompletion(CampaignDatabase database) => _database = database;
 
 		public CampaignLevel NextLevel() => _database.Levels.First();
+
+		// TODO: implement current level tracking
 		public CampaignLevel CurrentLevel() => _database.Levels.First();
+
+		public GameObject PlayerPrefab()
+		{
+			var activeScene = SceneManager.GetActiveScene();
+			if (activeScene.IsValid())
+			{
+				var level = _database.Find(activeScene.name);
+				if (level != null)
+				{
+					return level.PlayerPrefab ?? _database.PlayerPrefabFallback;
+				}
+			}
+
+			return _database.PlayerPrefabFallback;
+		}
 	}
 }
--- Runtime/CampaignDatabase.cs
+++ Runtime/CampaignDatabase.cs
@@ -2,6 +2,7 @@
 // © 2024-2025 Depra <n.melnikov@depra.org>
 
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static Depra.Campaign.Module;
 
@@ -11,9 +12,13 @@ namespace Depra.Campaign
 	public sealed class CampaignDatabase : ScriptableObject
 	{
 		[SerializeField] private CampaignLevel[] _levels;
+		[SerializeField] private GameObject _playerPrefabFallback;
 
 		private const string FILE_NAME = nameof(CampaignDatabase);
 
 		public IEnumerable<CampaignLevel> Levels => _levels;
+		public GameObject PlayerPrefabFallback => _playerPrefabFallback;
+
+		public CampaignLevel Find(string sceneName) => _levels.FirstOrDefault(level => level.SceneName == sceneName);
 	}
 }
